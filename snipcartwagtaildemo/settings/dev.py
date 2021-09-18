01bf7c7a8b3f7b96ff3337c7724906e96352a81a
Mister_Bert0ni <corpofhack@gmail.com>
2021-09-18 16:13:36 +0300

    Test Deployment

--- snipcartwagtaildemo/settings/dev.py
+++ snipcartwagtaildemo/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'nc6+zr9^1z$#86a%talc!tw0@y066shugmt@2@_14=an^nr$bi'
